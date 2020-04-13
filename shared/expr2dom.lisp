6c027163191b5c7e24ce31788d8633a68d77ede8
Sven Michael Klose <pixel@hugbox.org>
2020-04-13 09:29:59 +0200

    Revert last commit.
    PHP back end gets some clean ups for arrays.

--- shared/expr2dom.lisp
+++ shared/expr2dom.lisp
@@ -49,7 +49,7 @@
            (dom2expr-list x.next-sibling)))))
 
 (fn dom2expr-array (x)
-  (aprog1 #()
+  (aprog1 (make-array)
     (let k 0
       (dolist (v (dom2expr-list x.first-child))
         (= (aref ! k) v)
